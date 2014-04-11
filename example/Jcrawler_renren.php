408657171fd498924989ac4ca1df169784a4575b
Joe <thendfeel@gmail.com>
2014-04-11 15:31:48 +0800

    Jcrawler_renren.php

--- example/Jcrawler_renren.php
+++ example/Jcrawler_renren.php
@@ -3,8 +3,9 @@
  * 人人数据爬取分析
  * 本程序可根据cookie爬取人人网用户信息
  * 本例使用phpQuery.php作为html分析程序， 以DataSource.html文件为目标源文件
+ * 本例中用到的其他3个类文件均可在此项目中找到 https://github.com/thendfeel/xxtime
  *
- * @package     xxtime/Simple/Jcrawler_renren.php
+ * @package     xxtime/example/Jcrawler_renren.php
  * @author      joe@xxtime.com
  * @link        https://github.com/thendfeel/xxtime
  * @example     http://dev.xxtime.com
@@ -17,8 +18,13 @@ include '../phpQuery.php';
 include '../Jutil.php';
 include '../Jcurl.php';
 
-Jcurl::$cookie = 'anonymid=hqhua3nr-jbl05x; _r01_=1; JSESSIONID=abc-whI8Y7-UEdaQlnVqu; _urm_229165395=175935; timestamp=1392363322410; sign=2B4E0568DFB79B57307553544193ECBB; orignalSign=10a86cff6ad8895f07311796d9561073app_key12614735timestamp139236332241010a86cff6ad8895f07311796d9561073; depovince=GW; jebe_key=33c9afc4-38cd-494e-bd85-8287e2a80f89%7Cc4e0ea06c7f9f2bc5879e7a55a15104e%7C1393229670408%7C1; XNESSESSIONID=3f245656a8f1; ick_login=5d343dab-ba1d-4df7-920b-83de4ede785d; ick=2acb2169-fe6c-4e7c-8337-2e35b351b1c6; jebecookies=b9f8444e-f687-4584-b7fb-a83066c85541|||||; _de=52D969BB6AA78EF7CDC3323DFA47FB42; p=54c3d3ac5c6cddce191c761389a475515; t=f68dc9393363c26f9a9548a8a5d8750a5; societyguester=f68dc9393363c26f9a9548a8a5d8750a5; id=229165395; xnsid=97ee616f; loginfrom=null; feedType=229165395_hot';
+$jcurl = new Jcurl();
+$jcurl->cookie = 'anonymid=hqhua3nr-jbl05x; _r01_=1; depovince=GW; JSESSIONID=abcLxMmIirzkU-KXgPovu; jebe_key=33c9afc4-38cd-494e-bd85-8287e2a80f89%7Ccfcd208495d565ef66e7dff9f98764da%7C1397183774814%7C0%7C1397183736794; jebecookies=46dd6b07-dffb-41fa-a999-95a487c1a7d6|||||; ick_login=d9216c76-18db-4bda-ab78-5ae406ac0f6c; _de=52D969BB6AA78EF7CDC3323DFA47FB42; p=1248f9b4ccd20020a9b995b3dfe055f25; first_login_flag=1; t=06fdc7e984837ec380c5c007d1d8550c5; societyguester=06fdc7e984837ec380c5c007d1d8550c5; id=229165395; xnsid=b077ba43; loginfrom=syshome';
 $url = 'http://www.renren.com/238209939/profile?v=info_ajax';
+
+// $data = $jcurl->get($url); //获取在线数据
+// $doc = phpQuery::newDocument($data); //装载数据
+
 phpQuery::newDocumentFileHTML('DataSource.html');
 
 $input['plat'] = 'renren';
@@ -33,8 +39,8 @@ $input['mobile'] = pq('dl#profile_info dd:eq(2)')->text();
 $input['email'] = '';
 $input['site'] = pq('dl#profile_info a')->attr('href');
 $input['photo'] = pq('img#userpic')->attr('src');
-$input['hometown'] = pq('div#basicInfo dd:last a:first')->text() .' '. pq('div#basicInfo dd:last a:eq(1)')->text();
+$input['hometown'] = pq('div#basicInfo dd:last a:first')->text() . ' ' . pq('div#basicInfo dd:last a:eq(1)')->text();
 $input['college'] = pq('div#educationInfo dd a:first')->text();
 $input['interest'] = pq('td.userProfileItemValue a:first')->text();
-Jcurl::writeFile($input['realname'], 'result.txt');
+write_file($input['realname'], 'result.txt');
 dbx($input);
